c0c5505e2ebd7bf1f3185301cbecda0b8567d363
BOUKEZZOULA Fouzi <fboukezzoula@finaxys.com>
2017-06-25 01:35:40 +0200

    add CLI Docker and wildcard SSl

--- jenkins-master/jenkins.sh
+++ jenkins-master/jenkins.sh
@@ -2,7 +2,27 @@
 
 : "${JENKINS_HOME:="/var/jenkins_home"}"
 
+JENKINS_WAR=/usr/share/jenkins/jenkins.war 
+CONTROLPORT=8001
+
+HTTPS_PORT=2017
+HTTPS_HOST=0.0.0.0
+
+SSL_KEYSTORE=$JENKINS_HOME/.ssl/wildcard.finaxys-lab.intra.jks
+SSL_KEYSTORE_PASSWORD=24WhNone09
+
 JAVA_OPTS="-Dhudson.model.ParametersAction.keepUndefinedParameters=true -Djenkins.install.runSetupWizard=false -Djava.io.tmpdir=$JENKINS_HOME/tmp -Djsse.enableSNIExtension=false -Djava.awt.headless=true -server -XX:+AlwaysPreTouch -XX:+UseG1GC -XX:+ExplicitGCInvokesConcurrent -XX:+ParallelRefProcEnabled -XX:+UseStringDeduplication -XX:+UnlockDiagnosticVMOptions -XX:G1SummarizeRSetStatsPeriod=1 -Xms1024M -Xmx3G"
+JENKINS_OPTS="-jar $JENKINS_WAR --httpPort=-1 --httpsPort=$HTTPS_PORT --httpsListenAddress=$HTTPS_HOST --ajp13Port=-1 --httpsKeyStore=$SSL_KEYSTORE --httpsKeyStorePassword=$SSL_KEYSTORE_PASSWORD --controlPort=$CONTROLPORT"
+
+FILE="/var/jenkins_home/.init.done"
+
+if [ -f "$FILE" ]
+then
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+	eval "exec java $JAVA_OPTS $JENKINS_OPTS \"\$@\""
+fi
+exec "$@"
+else
 
 touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
@@ -10,27 +30,14 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c '. /usr/local/bin/jenkins-sup
 
 #sed -i 's/##ADMIN_JENKINS##/'"$ADMIN_JENKINS"'/g' "/var/jenkins_home/config.xml"
 
-# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
-if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
 
 mkdir -p $JENKINS_HOME/tmp
-
-  # read JAVA_OPTS and JENKINS_OPTS into arrays to avoid need for eval (and associated vulnerabilities)
-  java_opts_array=()
-  while IFS= read -r -d '' item; do
-    java_opts_array+=( "$item" )
-  done < <([[ $JAVA_OPTS ]] && xargs printf '%s\0' <<<"$JAVA_OPTS")
-
-  jenkins_opts_array=( )
-  while IFS= read -r -d '' item; do
-    jenkins_opts_array+=( "$item" )
-  done < <([[ $JENKINS_OPTS ]] && xargs printf '%s\0' <<<"$JENKINS_OPTS")
-
-  mkdir -p $JENKINS_HOME/tmp
-
-  exec java "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@"
+touch "$FILE"
+  
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+        eval "exec java $JAVA_OPTS $JENKINS_OPTS \"\$@\""
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for example a `bash` shell to explore this image
 exec "$@"
-
+fi
